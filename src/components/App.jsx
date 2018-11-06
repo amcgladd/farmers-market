6e094b643a98e375a2440c28a1091dd402e400f5
Student <example@example.com>
2018-11-06 13:00:03 -0800

    create project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Header from './Header';
-import TicketList from './TicketList';
+
 
 function App(){
-  var appStyles= {
-    backgroundColor: 'green'
-  }
   return (
-    <div style={appStyles}>
-      <Header/>
-      <TicketList />
+    <div>
     </div>
   );
 }
